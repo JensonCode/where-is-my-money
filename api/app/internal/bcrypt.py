76d3f891d92e5467221a174b93bf8abc69527587
JensonCode <lclcoding0714@hotmail.com>
2025-03-10 02:00:47 -0400

    hash and jwt

---
+++ api/app/internal/bcrypt.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Bcrypt:
+    def hash_password(password: str) -> str:
+        return pwd_context.hash(password)
+
+    def verify_password(password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(password, hashed_password)
