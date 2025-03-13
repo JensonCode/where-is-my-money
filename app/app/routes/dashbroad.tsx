5a665b2695ed6c4218c44c4fb58560b5d948334a
JensonCode <lclcoding0714@hotmail.com>
2025-03-13 01:46:27 -0400

    init dashbroad route

--- app/app/routes/dashbroad.tsx
+++ app/app/routes/dashbroad.tsx
@@ -1,3 +1,13 @@
+import { requireAuth } from '../utils/auth';
+import { useLoaderData } from '@remix-run/react';
+import { LoaderFunctionArgs } from '@remix-run/node';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const token = requireAuth(request);
+  return Response.json({ token });
+}
+
 export default function Dashbroad() {
-    return <div>dashbroad</div>
+  const token = useLoaderData<typeof loader>();
+  return <div>{token}</div>;
 }
